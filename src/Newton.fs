1bb826f6e4b2ec50f956adf8bdd1f274e0c4b8f6
ebresafegaga <ebresafegaga@outlook.com>
2020-09-07 22:34:14 +0100

    sexy syntax

--- src/Newton.fs
+++ src/Newton.fs
@@ -11,7 +11,7 @@ let rec repeat' f a = seq { yield a; yield! repeat' f (f a) }
 let rec repeat f a = Cons (lazy a, lazy repeat f (f a))
 
 let rec within eps = function  
-    | Cons (Lazy a, Lazy (Cons (Lazy b, rest))) -> 
+    Cons (Lazy a, Lazy (Cons (Lazy b, rest))) -> 
         if abs (a -. b) <= eps 
         then b
         else within eps (Cons (lazy b, rest))
@@ -19,7 +19,7 @@ let rec within eps = function
 let mysqrt a0 eps n = within eps (repeat (next n) a0)
 
 let rec relative eps = function
-    | Cons (Lazy a, Lazy (Cons (Lazy b, rest))) -> 
+    Cons (Lazy a, Lazy (Cons (Lazy b, rest))) -> 
         if abs (a/b -. 1.) <= eps 
         then b
         else relative eps (Cons (lazy b, rest))

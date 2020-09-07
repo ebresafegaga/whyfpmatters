3788698ae02d291b6395c0e735c640a22a2a1fef
ebresafegaga <ebresafegaga@outlook.com>
2020-09-07 22:44:58 +0100

    Updated repr for lazy lists.

--- src/Diff.fs
+++ src/Diff.fs
@@ -1,1 +1,13 @@
 module Diff
+
+type 'a listof = Nil | Cons of 'a Lazy * 'a listof
+
+let rec foldr f x = function 
+    | Nil -> x
+    | Cons (Lazy a, l) -> f a (foldr f x l)
+
+let map f l = foldr (f >> (fun a b -> (Cons (a, b)))) Nil l
+
+let rec repeat f a = Cons (lazy a, repeat f (f a))
+
+let easydiff f (x:float) h = (f (x+h) - f x)/h
--- src/Newton.fs
+++ src/Newton.fs
@@ -1,6 +1,6 @@
 module Newton
 
-type 'a listof = Nil | Cons of 'a Lazy * 'a listof Lazy
+type 'a listof = Nil | Cons of 'a Lazy * 'a listof
 
 let (-.) (a:float) b = a - b
 
@@ -8,10 +8,10 @@ let next n x = (x + float n/float x)/2.
 
 let rec repeat' f a = seq { yield a; yield! repeat' f (f a) }
 
-let rec repeat f a = Cons (lazy a, lazy repeat f (f a))
+let rec repeat f a = Cons (lazy a, repeat f (f a))
 
 let rec within eps = function  
-    Cons (Lazy a, Lazy (Cons (b, rest))) -> 
+    Cons (Lazy a, Cons (b, rest)) -> 
         if abs (a -. b.Value) <= eps 
         then b.Value
         else within eps (Cons (b, rest))
@@ -19,7 +19,7 @@ let rec within eps = function
 let mysqrt a0 eps n = within eps (repeat (next n) a0)
 
 let rec relative eps = function
-    Cons (Lazy a, Lazy (Cons (b, rest))) -> 
+    Cons (Lazy a, Cons (b, rest)) -> 
         if abs (a/b.Value -. 1.) <= eps 
         then b.Value
         else relative eps (Cons (b, rest))

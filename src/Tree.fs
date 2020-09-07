478120eab4ae202f17a7b1f9aad9820b036c42b4
ebresafegaga <ebresafegaga@outlook.com>
2020-09-07 21:31:59 +0100

    Project init.

---
+++ src/Tree.fs
@@ -0,0 +1,38 @@
+module Tree 
+
+
+type Tree<'a> = Node of 'a * Tree<'a> | Sub of Tree<'a> list
+
+
+let a = 
+    Node (1, 
+            Sub [ Node (2, Sub [])
+                  Sub [ Node (3, Sub [ Node (4, Sub [])])]
+                ])
+
+let rec foldtree f g a t = 
+    match t with
+    | Node (label, subtrees) ->
+        f label (foldtree f g a subtrees)
+    | Sub (x :: xs) ->
+        g (foldtree f g a x) (foldtree f g a (Sub xs))
+    | Sub [] -> a
+
+let result = foldtree (+) (+) 0 a
+
+(* Don't try this at home *)
+let Node a as' = Node (a, as')
+let (++) x xs =
+    match xs with
+    | Sub ts -> Sub (x :: ts)
+    | Node _ -> failwith "you just can't"
+let empty = Sub []
+
+let maptree f = foldtree (Node << f) (++) empty 
+
+let result' = maptree ((+) 1) a
+
+type Id = Id of int * int 
+
+let abx = Id (12, 12)
+let (Id (dd, _)) = abx

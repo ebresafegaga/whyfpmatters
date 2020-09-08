d4853e8b5bf0acaed531504aa6b515d1d7527ee6
ebresafegaga <ebresafegaga@outlook.com>
2020-09-08 01:16:56 +0100

    Changed repr for lazy lists, again

--- src/Diff.fs
+++ src/Diff.fs
@@ -1,13 +1,20 @@
 module Diff
 
-type 'a listof = Nil | Cons of 'a Lazy * 'a listof
+type 'a listof = Nil | Cons of 'a Lazy * 'a listof Lazy
 
+let (|Cons'|) = function
+    Cons (Lazy a, Lazy rest) -> Cons' (a, rest)
+  
 let rec foldr f x = function 
     | Nil -> x
-    | Cons (Lazy a, l) -> f a (foldr f x l)
+    | Cons' (a, l) -> f a (foldr f x l)
 
-let map f l = foldr (f >> (fun a b -> (Cons (a, b)))) Nil l
+let map f l = foldr (fun a b -> (Cons (lazy f a, lazy b))) Nil l
 
-let rec repeat f a = Cons (lazy a, repeat f (f a))
+let rec repeat f a = Cons (lazy a, lazy repeat f (f a))
 
 let easydiff f (x:float) h = (f (x+h) - f x)/h
+
+
+let halve x = x/2.
+let differentiate h0 f x = map (easydiff f x) (repeat halve h0)
